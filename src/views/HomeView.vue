3a759c81f1ebd93a2118de3343b01a3c2e6d96fd
Wenqi <ytj168200750@gmail.com>
2024-07-03 11:53:15 +0800

    yee

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
 </script>
 
 <template>
-  <main>
-    <h1>111</h1>
-    <TheWelcome />
-  </main>
+  
 </template>
